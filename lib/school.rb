033ae29fce5b656ba688a39a19b5b2effe8cae36
Jimmy Millard <millardjimmy@gmail.com>
2020-05-15 18:26:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,24 @@
 class School
+  def intialize(name)
+    @name = name
+    @roster = {}
 end
+
+def roster
+  @roster
+end
+
+def add_student(name, grade)
+  if @roster[grade] != nil 
+    @roster[grade] << name 
+  else
+    @roster[grade] = [name]
+  end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort 
+  @roster.each do
